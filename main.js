2d083e09e6df6c82da3bf11ac5cbec27fb0ea88c
mobinzxr <105461009+mobinzxr@users.noreply.github.com>
2023-01-18 13:21:16 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+
+
+    let yearnow = 1401
+    let monthnow = 10
+    let daynow = 28
+    let yearOfBirthday = document.querySelector(".userYearBirth")
+    let monthOfBirthday = document.querySelector(".userMonthBirth")
+    let dayOfBirthday = document.querySelector(".userDayBirth")
+    let todayDate = document.getElementById("dateNow")
+    yearOfBirthday = Number(yearOfBirthday.value)
+    monthOfBirthday = Number(monthOfBirthday.value)
+    dayOfBirthday = Number(dayOfBirthday.value)
+    todayDate.innerHTML = "تاریخ امروز:"+yearnow+"/"+monthnow+"/"+daynow
+    let passYear = yearnow - yearOfBirthday
+    let passMonth = monthnow - monthOfBirthday
+    let passDay = daynow - dayOfBirthday
+ function convert(){
+    if (passMonth <= 0) {
+        passYear = passYear - 1
+        passMonth = 12 + passMonth
+    }
+    if (passDay <= 0) {
+        passMonth = passMonth - 1
+        passDay = 30 + passDay
+    }
+    if(passMonth === 12){
+        passYear++;
+        passMonth = 0;
+    }
+    console.log(passYear);
+    console.log(passMonth);
+    console.log(passDay);
+    console.log(yearOfBirthday);
+ }
+    
